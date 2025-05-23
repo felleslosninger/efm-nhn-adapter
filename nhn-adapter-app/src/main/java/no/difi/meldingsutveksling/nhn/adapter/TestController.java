89b81799cc281ac97ab88263bb781833493926e0
Alexander Petrov <alexander.petrov@digdir.no>
2025-05-23 10:07:36 +0200

    vault + test helseIdClientConfig

--- nhn-adapter-app/src/main/java/no/difi/meldingsutveksling/nhn/adapter/TestController.java
+++ nhn-adapter-app/src/main/java/no/difi/meldingsutveksling/nhn/adapter/TestController.java
@@ -1,14 +1,26 @@
 package no.difi.meldingsutveksling.nhn.adapter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import no.ks.fiks.helseid.HelseIdClient;
+
 @RestController
 public class TestController {
+    
+    @Autowired
+    private HelseIDClientConfig config;
+    
+    private HelseIdClient helseIdClient;
 
 	@GetMapping(path = "/hello")
 	public String test() {
-		return "Hello World";
+		return "Hello World" + config.getClientID().length() + "," + config.getJwkKey().length();
+	}
+	@GetMapping(path = "/testHelseId")
+	public String testHelseId() {
+	   return helseIdClient.getAccessToken().getAccessToken();
 	}
 
 }
