b48752e7766e9700bb132a0ac66e0f1af223b2ca
Alexander Petrov <alexander.petrov@digdir.no>
2025-07-04 13:51:41 +0200

    test FLR/AR brukeren vi fikk
    Co-authored-by: Håvard England <hme@digdir.no>

--- nhn-adapter-kt-app-4/src/main/kotlin/no/difi/meldingsutveksling/nhn/adapter/BeanRegistration.kt
+++ nhn-adapter-kt-app-4/src/main/kotlin/no/difi/meldingsutveksling/nhn/adapter/BeanRegistration.kt
@@ -2,6 +2,11 @@ package no.difi.meldingsutveksling.nhn.adapter
 
 import com.sun.source.tree.CompilationUnitTree
 import kotlinx.serialization.Serializable
+import no.ks.fiks.nhn.ar.AdresseregisteretClient
+import no.ks.fiks.nhn.flr.Credentials
+import no.ks.fiks.nhn.flr.Environment
+import no.ks.fiks.nhn.flr.FastlegeregisteretClient
+import no.nhn.schemas.reg.flr.IFlrReadOperations
 import org.springframework.beans.factory.BeanRegistrarDsl
 import org.springframework.http.codec.KotlinSerializationSupport
 import org.springframework.stereotype.Component
@@ -44,6 +49,22 @@ class BeanRegistration : BeanRegistrarDsl ({
             POST("/dph/out") {
                 val messageOut = it.awaitBody<MessageOut>()
                 println("MessageOut recieved ${messageOut.conversationId}")
+                ServerResponse.ok().buildAndAwait()
+            }
+
+            POST("/flr/test") {
+                val flrClinet = FastlegeregisteretClient(Environment.TEST, Credentials(username = "****", password = "*****"))
+                val patientGP = flrClinet.getPatientGP("30905895733")
+                println(patientGP?.gpHerId)
+                ServerResponse.ok().buildAndAwait()
+            }
+
+            POST("/ar/test") {
+                val arClient = AdresseregisteretClient(
+                    no.ks.fiks.nhn.ar.Environment.TEST,
+                    no.ks.fiks.nhn.ar.Credentials(username = "*****", password = "*****"))
+                val reciever = arClient.lookupHerId(8143060)
+                println(reciever?.name)
                 ServerResponse.ok().buildAndAwait()
             }
         }
