80d04cb7e756d55f74e60f09be49492f95d29e5d
Alexander Petrov <alexander.petrov@digdir.no>
2026-01-19 14:56:35 +0100

    cause skall vare throwable

--- nhn-crypto/src/main/kotlin/no/difi/meldingsutveksling/nhn/adapter/crypto/EncryptionException.kt
+++ nhn-crypto/src/main/kotlin/no/difi/meldingsutveksling/nhn/adapter/crypto/EncryptionException.kt
@@ -1,3 +1,3 @@
 package no.difi.meldingsutveksling.nhn.adapter.crypto
 
-class EncryptionException(override val message: String, e: Exception? = null) : Exception(message, e)
+class EncryptionException(override val message: String, e: Throwable? = null) : Exception(message, e)
